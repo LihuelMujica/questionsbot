7fae9deb1eac0201c63995961df3d9093c759f66
Lihuel <lihuelmujica@gmail.com>
2022-12-24 15:30:52 -0300

    first commit

---
+++ src/main/java/com/lihuel/questionsbot/QuestionsBotApplication.java
@@ -0,0 +1,13 @@
+package com.lihuel.questionsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lihuel/questionsbot/QuestionsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihuel.questionsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
